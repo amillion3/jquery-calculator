018c65cfe86ac5d487072eb26aa35670d53a5837
Andy <amillion3@gmail.com>
2018-05-08 20:39:25 -0500

    event listeners complete for now

--- javascripts/events.js
+++ javascripts/events.js
@@ -1,11 +1,48 @@
+// Begin NUMBER functionality
+const clickedNumber = e => {
+  const buttonClicked = e.target.id * 1;
+};
+
+// Begin MATH functionality
+const clickedMath = e => {
+  const buttonClicked = e.target.id;
+  if (buttonClicked === 'math-plus') {
+
+  } else if (buttonClicked === 'math-minus') {
+
+  } else if (buttonClicked === 'math-multiply') {
+
+  } else {
+
+  }
+};
+
+// Begin CLEAR functionality
+const clearSingle = () => {
+
+};
+
+const clearAll = () => {
+
+};
+
+const clickedClear = e => {
+  const buttonClicked = e.target.id;
+  if (buttonClicked === 'clear-single') {
+    clearSingle();
+  } else {
+    clearAll();
+  }
+};
+
 const btnClicked = e => {
   const buttonClicked = $(e.target).closest('.btn-calc')[0];
   if ($(buttonClicked).hasClass('btn-number')) {
-    console.log('number');
+    clickedNumber(e);
   } else if ($(buttonClicked).hasClass('btn-math')) {
-    console.log('math');
+    clickedMath(e);
   } else if ($(buttonClicked).hasClass('btn-clear')) {
-    console.log('clear');
+    clickedClear(e);
   }
 };
 
